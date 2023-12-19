82addd05ba7a0f8d2a5ac7d3d536244335bfd032
Gabs343 <gabriellopezp343@gmail.com>
2023-12-19 17:26:19 -0300

    testing dictionary setting and updating the database

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Main:
     __status_callback = None
     
     def __init__(self) -> None:
-        self.__settings_services = [setting() for setting in (SettingBot,)]
+        self.__settings_services = [setting(bot_name=self.__bot_name) for setting in (BotSetting, TaskManagerSetting)]
         
     @property   
     def settings_services(self) -> list[SettingService]:
@@ -38,15 +38,14 @@ class Main:
         self.__status_callback = callback
         
     def start(self) -> None:
-        self.__logs_services = [log() for log in (LogTxt, LogXlsx)]
-        self.__notify_status(new_status="RUNNING")
+        self.__execution_begun()
         
         #Your code goes here
         
         ####################
           
-        self.__notify_status(new_status="READY")
-        
+        self.__execution_completed()
+                
     def pause(self):
         self.__notify_status(new_status='PAUSED')
             
@@ -56,23 +55,47 @@ class Main:
     def stop(self):
         self.__notify_status(new_status='CLOSING BOT')
         
+    def __execution_begun(self) -> None:
+        self.__logs_services = [log() for log in (LogTxt, LogXlsx)]
+        bot_setting_service: BotSetting = self.__get_setting_service(setting_type=BotSetting)
+        bot_setting_service.settings['executions'] += 1
+        self.__notify_status(new_status="RUNNING")
+             
+    def __execution_completed(self, had_error: bool = False):
+        bot_setting_service: BotSetting = self.__get_setting_service(setting_type=BotSetting)
+        if(had_error):  
+            bot_setting_service.settings['bad_executions'] += 1
+        else:
+            bot_setting_service.settings['good_executions'] += 1
+            
+        bot_setting_service.update()
+        self.__close_logs()
+        self.__notify_status(new_status="READY")
+        
     def __notify_status(self, new_status: str) -> None:
         self.__status = new_status
-        logTxt: LogTxt = self.__get_log(log_type=LogTxt)
+        logTxt: LogTxt = self.__get_log_service(log_type=LogTxt)
         logTxt.write_info(message=f'Bot {new_status}')
         if self.__status_callback:
             self.__status_callback(new_status)
         
-    def __get_log(self, log_type: LogService) -> LogService:
+    def __get_log_service(self, log_type: LogService) -> LogService:
         return next(log for log in self.__logs_services if isinstance(log, log_type))
     
+    def __get_setting_service(self, setting_type: SettingService) -> SettingService:
+        return next(service for service in self.__settings_services if isinstance(service, setting_type))
+    
     def __execute_action(self, function, *args):
-        logTxt: LogTxt = self.get_log(log_type=LogTxt)
+        logTxt: LogTxt = self.__get_log_service(log_type=LogTxt)
         if(self.__status == 'PAUSED'):
             while True:
                 if(self.__status=='RUNNING'):
                     break
         return logTxt.write_and_execute(function, *args)
+    
+    def __close_logs(self) -> None:
+        for log in self.__logs_services:
+            log.close()
                        
             
 if __name__ == "__main__":
