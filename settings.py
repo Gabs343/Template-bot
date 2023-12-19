08c1900bcb87c116667341e2707604c1952b07b5
Gabs343 <gabriellopezp343@gmail.com>
2023-12-19 17:43:54 -0300

    using the variable repository to call the database

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+from bot_db import *
+
 class SettingService: 
     def __init__(self):
         self.__settings: dict = {}
@@ -10,12 +12,54 @@ class SettingService:
     def settings(self, setting) -> None:
         self.__settings = setting
 
-class SettingBot(SettingService):
+class BotSetting(SettingService):
     
-    def __init__(self) -> None:
+    __repository: SettingTable
+    
+    def __init__(self, bot_name: str) -> None:
         super().__init__()
-        self.settings = dict.fromkeys(["key_one"])
-        self.settings["key_one"] = "test"
+        self.settings = self.get_new_settings()
+        self.__repository = SettingTable(bot_name=bot_name, 
+                                         setting_name=self.__str__(),
+                                         settings=self.settings)
+        self.__repository.create()
+        self.settings = self.__repository.get()
+        
+        self.settings = self.__repository.get()
+        
+    def get_new_settings(self) -> dict:
+        setting: dict = dict.fromkeys(['executions', 'good_executions', 'bad_executions'])
+        setting['executions'] = 0
+        setting['good_executions'] = 0
+        setting['bad_executions'] = 0
+        return setting
+        
+    def update(self) -> None:
+        self.__repository.update(data=self.settings)
         
     def __str__(self) -> str:
-        return "Setting Bot"
+        return type(self).__name__
+    
+class TaskManagerSetting(SettingService):
+    
+    __repository: SettingTable
+    
+    def __init__(self, bot_name: str):
+        super().__init__()
+        self.settings = self.get_new_settings()
+        self.__repository = SettingTable(bot_name=bot_name, 
+                                         setting_name=self.__str__(),
+                                         settings=self.settings)
+        self.__repository.create()
+        self.settings = self.__repository.get()
+        
+    def get_new_settings(self) -> dict:
+        setting: dict = dict.fromkeys(['task_name'])
+        setting['task_name'] = 'None'
+        return setting
+    
+    def update(self) -> None:
+        self.__repository.update(data=self.settings)
+    
+    def __str__(self) -> str:
+        return type(self).__name__
