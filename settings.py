570bddcc14c87148ea8d827d9dd9e64fe4ded58b
Gabs343 <gabriellopezp343@gmail.com>
2023-12-22 12:44:11 -0300

    functions to create, edit and delete a task for the task scheduler

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+import subprocess
 from bot_db import *
 
 class SettingService: 
@@ -50,13 +51,58 @@ class TaskManagerSetting(SettingService):
         self.__repository.create()
         self.settings = self.__repository.get()
         
+    def __get_path(self) -> str:
+        path: str = os.path.abspath(__file__).split("\\")[0:-1]
+        path = '\\'.join(path)
+        return path
+    
     def get_new_settings(self) -> dict:
         setting: dict = dict.fromkeys(['task_name'])
         setting['task_name'] = 'None'
+        setting['start_time'] = ''
         return setting
     
     def update(self) -> None:
         self.__repository.update(data=self.settings)
+        
+    def __get_python_path(self) -> str:
+        command: str = 'where python'
+        return subprocess.run(command, 
+                              shell=True, 
+                              stdout=subprocess.PIPE, 
+                              stderr=subprocess.PIPE).stdout.decode("utf-8").strip()
+        
+    def __task_exists(self) -> bool:
+        command: str = f'schtasks /query /tn {self.settings["task_name"]}'
+        result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return result.returncode == 0
+    
+    def create_scheduled_task(self) -> str:
+        if (self.__task_exists()):
+            return f'Task "{self.settings["task_name"]}" already exists. Please choose a different name.'
+        else:
+            task_run: str = f'cmd /c cd {self.__get_path()} && {self.__get_python_path()} main.py'
+            command: str = f'schtasks /create /sc daily /tn {self.settings["task_name"]} /tr "{task_run}" /st {self.settings["start_time"]}'
+            subprocess.run(command, shell=True)
+            return f'Scheduled task "{self.settings["task_name"]}" created successfully.'
+        
+    def edit_scheduled_task(self) -> str:
+        if (self.__task_exists()):
+            task_run: str = f'cmd /c cd {self.__get_path()} && {self.__get_python_path()} main.py'
+            command: str = f'schtasks /change /tn {self.settings["task_name"]} /tr "{task_run}" /st {self.settings["start_time"]}'
+            subprocess.run(command, shell=True)
+            return f'Scheduled task "{self.settings["task_name"]}" edited successfully.'
+        else:
+            return f'Task {self.settings["task_name"]} does not exist. Cannot edit.'
+            
+    def delete_scheduled_task(self) -> str:
+        if (self.__task_exists()):
+            command: str = f'schtasks /delete /tn {self.settings["task_name"]} /f'
+            subprocess.run(command, shell=True)
+            return f'Scheduled task "{self.settings["task_name"]}" deleted successfully.'
+        else:
+            return f'Task "{self.settings["task_name"]}" does not exist. Cannot delete.'
+
     
     def __str__(self) -> str:
         return type(self).__name__
